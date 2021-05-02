f11c1278642401c2587697e2fbd236ca7093e62f
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 00:43:16 -0400

    add state property

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PersonList from './components/PersonList';
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {}
+  
   render() {
     return (
       <div className="App">
