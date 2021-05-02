5446f10b9615b7a7e867dd5b0cc1e1393adcb496
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 00:09:01 -0400

    Write the first test proving the App component is rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders learn react link', () => {
-  render(<App />);
-  
+describe('App', () => {
+  it('renders the App component', () => {
+    const appWrapper = shallow(<App />);
+  });  
 });
