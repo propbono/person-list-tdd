515c8faf48699e7f6c847c31d88202eea9746a22
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 01:05:29 -0400

    t App should pass prop to the PersonList

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-import PersonList from './components/PersonList';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import PersonList from "./components/PersonList";
 
-describe('App', () => {
-
-  let appWrapper; 
+describe("App", () => {
+  let appWrapper;
 
   beforeAll(() => {
     appWrapper = shallow(<App />);
   });
 
-  it('should render Person List Component', () => {
+  it("should render Person List Component", () => {
     appWrapper.find(PersonList);
   });
 
-  it('should have only one person list component', () => {
+  it("should have only one person list component", () => {
     const personList = appWrapper.find(PersonList);
 
     expect(personList).toHaveLength(1);
   });
 
-  it('should have not null state', () => {
+  it("should have not null state", () => {
     const appState = appWrapper.state();
 
     expect(appState).not.toBeNull();
   });
 
-  it('should have people property on the state', () => {
+  it("should have people property on the state", () => {
     const appState = appWrapper.state();
-    
+
     expect(appState.people).toBeDefined();
+  });
+
+  it("should pass people props to PersonList", () => {
+    const personList = appWrapper.find(PersonList);
+
+    expect(personList.props().people).toEqual(appWrapper.state().people);
   });
 });
