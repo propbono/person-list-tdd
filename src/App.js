0ea5ab1b3adbeb197f4b2ab9705bbcf47a29e29a
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 01:05:42 -0400

    Pass people prop to Person List

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import PersonList from './components/PersonList';
-import React, { Component } from 'react';
+import "./App.css";
+import PersonList from "./components/PersonList";
+import React, { Component } from "react";
 
 class App extends Component {
-  state = { people: []}
+  state = { people: [] };
 
   render() {
     return (
       <div className="App">
-        <PersonList />
+        <PersonList people={this.state.people} />
       </div>
     );
   }
