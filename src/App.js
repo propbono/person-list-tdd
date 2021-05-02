383274720648fbeba7b5ffee323378f854346b97
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 00:48:54 -0400

    add people prop to the state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import PersonList from './components/PersonList';
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {}
-  
+  state = { people: []}
+
   render() {
     return (
       <div className="App">
