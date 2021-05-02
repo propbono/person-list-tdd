acbf33c45cdaeabf2ef41188e3b220bf8fb4ce68
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 00:16:45 -0400

    Add a PersonList Render test and make it pass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PersonList from './components/PersonList';
 
 function App() {
   return (
     <div className="App">
-     
+      <PersonList />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PersonList from './components/PersonList';
 
 describe('App', () => {
   it('renders the App component', () => {
     const appWrapper = shallow(<App />);
   });  
+
+  it('should render Person List Component', () => {
+    const appWrapper = shallow(<App />);
+    appWrapper.find(<PersonList />);
+  });
 });
