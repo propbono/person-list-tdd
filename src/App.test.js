1de0b9de8620f0883a671ff7c23e2679848b99b7
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 00:45:21 -0400

    refactor test and add appWrapper in before all test

--- src/App.test.js
+++ src/App.test.js
@@ -4,29 +4,32 @@ import App from './App';
 import PersonList from './components/PersonList';
 
 describe('App', () => {
+
+  let appWrapper; 
+
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+  });
+
   it('renders the App component', () => {
     const appWrapper = shallow(<App />);
   });  
 
   it('should render Person List Component', () => {
-    const appWrapper = shallow(<App />);
     appWrapper.find(PersonList);
   });
 
   it('should have only one person list component', () => {
-    const appWrapper = shallow(<App />);
     const personList = appWrapper.find(PersonList);
 
     expect(personList).toHaveLength(1);
   });
 
   it('should have a state', () => {
-    const appWrapper = shallow(<App />);
     const appState = appWrapper.state();
   });
 
   it('should have not null state', () => {
-    const appWrapper = shallow(<App />);
     const appState = appWrapper.state();
 
     expect(appState).not.toBeNull();
