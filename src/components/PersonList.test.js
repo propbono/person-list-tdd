08673237f06c9fcc7d85c6de44a67bf05bef5301
Greg Mozer <mozer.grzegorz@yahoo.com>
2021-05-02 01:40:11 -0400

    t check if people is 0 or 1

--- src/components/PersonList.test.js
+++ src/components/PersonList.test.js
@@ -9,4 +9,20 @@ describe("PersonList", () => {
 
     expect(peopleUlList).toHaveLength(1);
   });
+
+  it("renders no li elements when pass empty people list", () => {
+    const people = [];
+    const personListWrapper = shallow(<PersonList people={people} />);
+    const peopleListItems = personListWrapper.find("li");
+
+    expect(peopleListItems).toHaveLength(0);
+  });
+
+  it("renders one li element when pass one person", () => {
+    const people = [{ firstName: "John", lastName: "Doe" }];
+    const personListWrapper = shallow(<PersonList people={people} />);
+    const peopleListItems = personListWrapper.find("li");
+
+    expect(peopleListItems).toHaveLength(1);
+  });
 });
